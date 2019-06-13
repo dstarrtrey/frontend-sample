1a92beda6859ebea77cd484602105472a19377d8
dstarrtrey <dstarrtrey@gmail.com>
2019-06-12 21:00:28 -0700

    Adds scss compatability to webpack

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import styles from './scss/app.module'
+import './scss/global'
+
+function App () {
+  return <h2 className={styles.red}>This is our React application!</h2>
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,64 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
 module.exports = {
   entry: {
-    main: './src/main.js'
+    main: './src/',
   },
   module: {
     rules: [
-      {
+      { // JS/JSX Compile to ES5
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+          loader: "babel-loader",
+        },
+      },
+      { // SASS/SCSS Modules
+        // For componentised styling
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localsConvention: 'camelCaseOnly',
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: false,
+            },
+          },
+        ],
+      },
+      { // Normal SASS/SCSS files (excluding modules)
+        // More for global styling
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css',
+    }),
+  ],
 };
